fb364cef9bef06cc420cccdcb8dcef23d08766f5
everett.fu <everett.fu@gmail.com>
2024-07-16 15:27:27 +0800

    style:使用结构体，并将绘制背景矩形的函数整合成一个函数

--- helloos/bootpack.c
+++ helloos/bootpack.c
@@ -7,6 +7,7 @@ void io_store_eflags(int eflags);
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+void init_screen(unsigned char *vram, int xsize, int ysize);
 
 #define COL8_000000 0            // 0: 黑
 #define COL8_FF0000 1            // 1: 亮红
@@ -25,39 +26,24 @@ void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, i
 #define COL8_008484 14            // 14: 浅暗蓝
 #define COL8_848484 15            // 15: 暗灰
 
+struct BOOTINFO {
+	char cyls, leds, vmode, reserve;
+	short scrnx, scrny;
+	unsigned char *vram;
+};
+
 void HariMain(void) {
 	unsigned char *vram;
 	int xsize, ysize;
-	short *binfo_scrnx, *binfo_scrny;
-	int *binfo_vram;
+	struct BOOTINFO *binfo;
 
 	init_palette();
-	binfo_scrnx = (short *)0x0ff4;
-	binfo_scrny = (short *)0x0ff6;
-	binfo_vram = (int *)0x0ff8;
-	xsize = *binfo_scrnx;
-	ysize = *binfo_scrny;
-	vram = (unsigned char *) *binfo_vram;
+	binfo = (struct BOOTINFO *) 0x0ff0;
+	xsize = (*binfo).scrnx;
+	ysize = (*binfo).scrny;
+	vram = (*binfo).vram;
 
-	// 绘制背景
-	boxfill8(vram, xsize, COL8_008484, 0, 0, xsize - 1, ysize - 29);
-	boxfill8(vram, xsize, COL8_C6C6C6, 0, ysize - 28, xsize - 1, ysize - 28);
-	boxfill8(vram, xsize, COL8_FFFFFF, 0, ysize - 27, xsize - 1, ysize - 27);
-	boxfill8(vram, xsize, COL8_C6C6C6, 0, ysize - 26, xsize - 1, ysize - 1);
-
-	//
-	boxfill8(vram, xsize, COL8_FFFFFF, 3, ysize - 24, 59, ysize - 24);
-	boxfill8(vram, xsize, COL8_FFFFFF, 2, ysize - 24, 2, ysize - 4);
-	boxfill8(vram, xsize, COL8_848484, 3, ysize - 4, 59, ysize - 4);
-	boxfill8(vram, xsize, COL8_848484, 59, ysize - 23, 59, ysize - 5);
-	boxfill8(vram, xsize, COL8_000000, 2, ysize - 3, 59, ysize - 3);
-	boxfill8(vram, xsize, COL8_000000, 60, ysize - 24, 60, ysize - 3);
-
-	//
-	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize - 4, ysize - 24);
-	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize - 4);
-	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize - 3, xsize - 4, ysize - 3);
-	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 3, ysize - 24, xsize - 3, ysize - 3);
+	init_screen(vram, xsize, ysize);
 
 	for (;;)
 		io_hlt();
@@ -133,4 +119,26 @@ void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, i
 		}
 	}
 	return;
+}
+
+void init_screen(unsigned char *vram, int xsize, int ysize) {
+	// 绘制背景
+	boxfill8(vram, xsize, COL8_008484, 0, 0, xsize - 1, ysize - 29);
+	boxfill8(vram, xsize, COL8_C6C6C6, 0, ysize - 28, xsize - 1, ysize - 28);
+	boxfill8(vram, xsize, COL8_FFFFFF, 0, ysize - 27, xsize - 1, ysize - 27);
+	boxfill8(vram, xsize, COL8_C6C6C6, 0, ysize - 26, xsize - 1, ysize - 1);
+
+	//
+	boxfill8(vram, xsize, COL8_FFFFFF, 3, ysize - 24, 59, ysize - 24);
+	boxfill8(vram, xsize, COL8_FFFFFF, 2, ysize - 24, 2, ysize - 4);
+	boxfill8(vram, xsize, COL8_848484, 3, ysize - 4, 59, ysize - 4);
+	boxfill8(vram, xsize, COL8_848484, 59, ysize - 23, 59, ysize - 5);
+	boxfill8(vram, xsize, COL8_000000, 2, ysize - 3, 59, ysize - 3);
+	boxfill8(vram, xsize, COL8_000000, 60, ysize - 24, 60, ysize - 3);
+
+	//
+	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 24, xsize - 4, ysize - 24);
+	boxfill8(vram, xsize, COL8_848484, xsize - 47, ysize - 23, xsize - 47, ysize - 4);
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 47, ysize - 3, xsize - 4, ysize - 3);
+	boxfill8(vram, xsize, COL8_FFFFFF, xsize - 3, ysize - 24, xsize - 3, ysize - 3);
 }
