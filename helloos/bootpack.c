206d359023edb29da50a6f9267e3a4ab242e3477
everett.fu <everett.fu@gmail.com>
2024-07-22 19:08:01 +0800

    feat:显示鼠标三位控制码

--- helloos/bootpack.c
+++ helloos/bootpack.c
@@ -58,30 +58,49 @@ void HariMain(void) {
 	sprintf(s, "(%d, %d)", mx, my);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
+	unsigned char mouse_dbuf[3], mouse_phase;
 	// 激活鼠标
 	enable_mouse();
+	mouse_phase = 0;
 
 	for (;;) {
 		io_cli();
 		// 判断是否有键盘输入
 		// 如果没有键盘输入，则进入休眠状态
-		if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo)== 0) {
+		if (fifo8_status(&keyfifo) + fifo8_status(&mousefifo) == 0) {
 			io_stihlt();
-		}
+		} else {
 			// 如果有键盘输入，则显示键盘输入
-		else if (fifo8_status(&keyfifo) != 0) {
-			i = fifo8_get(&keyfifo);
-			io_sti();
-			sprintf(s, "%02X", i);
-			boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
-			putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
-		}
-		else if (fifo8_status(&mousefifo) != 0) {
-			i = fifo8_get(&mousefifo);
-			io_sti();
-			sprintf(s, "%02X", i);
-			boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 47, 31);
-			putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+			if (fifo8_status(&keyfifo) != 0) {
+				i = fifo8_get(&keyfifo);
+				io_sti();
+				sprintf(s, "%02X", i);
+				boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
+				putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
+			}
+			// 如果有鼠标输入，则显示鼠标输入
+			else if (fifo8_status(&mousefifo) != 0) {
+				i = fifo8_get(&mousefifo);
+				io_sti();
+				if (mouse_phase == 0) {
+					if (i == 0xfa) {
+						mouse_phase = 1;
+					}
+				} else if (mouse_phase == 1) {
+					mouse_dbuf[0] = i;
+					mouse_phase = 2;
+				} else if (mouse_phase == 2) {
+					mouse_dbuf[1] = i;
+					mouse_phase = 3;
+				} else if (mouse_phase == 3) {
+					mouse_dbuf[2] = i;
+					mouse_phase = 1;
+
+					sprintf(s, "%02x %02x %02x", mouse_dbuf[0], mouse_dbuf[1], mouse_dbuf[2]);
+					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 8 * 8 - 1, 31);
+					putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+				}
+			}
 		}
 	}
 }
