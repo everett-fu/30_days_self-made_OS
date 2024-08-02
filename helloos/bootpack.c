89032994e73013163cee3736a9ee294e83224896
everett.fu <everett.fu@gmail.com>
2024-08-02 20:29:47 +0800

    feat:内存管理

--- helloos/bootpack.c
+++ helloos/bootpack.c
@@ -13,14 +13,35 @@
  */
 #include <stdio.h>
 #include "bootpack.h"
-unsigned int memtest(unsigned int start, unsigned int end);
-unsigned int memtest_sub(unsigned int start, unsigned int end);
+
+#define MEMMAN_FREES 4090
+#define MEMMAN_ADDR 0x003c0000
+
+// 可用内存信息
+struct FREEINFO {
+	// 内存地址，内存大小
+	unsigned int addr, size;
+};
+
+// 内存管理
+struct MEMMAN {
+	// 可用信息数量，最大frees数量，释放失败内存大小总和，释放失败次数
+	int frees, maxfrees, lostsize, losts;
+	struct FREEINFO free[MEMMAN_FREES];
+};
+
+void memman_init(struct MEMMAN *man);
+unsigned int memman_total(struct MEMMAN *man);
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
 
 void HariMain(void) {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
 	char s[40], mcursor[256], keybuf[32], mousebuf[128];
 	int mx, my, i;
 	struct MOUSE_DEC mdec;
+	unsigned int memtotal;
+	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
 
 	// 初始化GDT,IDT
 	init_gdtidt();
@@ -45,6 +66,14 @@ void HariMain(void) {
 	init_keyboard();
 	// 激活鼠标
 	enable_mouse(&mdec);
+	// 初始化内存管理
+	// 获取内存大小
+	memtotal = memtest(0x00400000, 0xbfffffff);
+	// 初始化内存管理
+	memman_init(memman);
+	memman_free(memman, 0x00001000,0x0009e000);
+	memman_free(memman, 0x00400000,memtotal - 0x00400000);
+
 
 	// 初始化调色板
 	init_palette();
@@ -63,8 +92,7 @@ void HariMain(void) {
 	sprintf(s, "(%d, %d)", mx, my);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
-	i = memtest(0x00400000, 0xbfffffff) / (1024 * 1024);
-	sprintf(s, "memory %dMB", i);
+	sprintf(s, "memory %dMB free : %dKB", memtotal / (1024 * 1024), memman_total(memman) / 1024);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
 
 	// 系统主循环
@@ -85,7 +113,7 @@ void HariMain(void) {
 				boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 16, 15, 31);
 				putfonts8_asc(binfo->vram, binfo->scrnx, 0, 16, COL8_FFFFFF, s);
 			}
-			// 如果有鼠标输入，则显示鼠标输入
+				// 如果有鼠标输入，则显示鼠标输入
 			else if (fifo8_status(&mousefifo) != 0) {
 				i = fifo8_get(&mousefifo);
 				io_sti();
@@ -176,3 +204,103 @@ unsigned int memtest(unsigned int start, unsigned int end) {
 	}
 	return i;
 }
+
+// 初始化内存设置
+void memman_init(struct MEMMAN *man) {
+	man->frees = 0;
+	man->maxfrees = 0;
+	man->lostsize = 0;
+	man->losts = 0;
+	return;
+}
+
+// 统计剩余内存总大小
+unsigned int memman_total(struct MEMMAN *man) {
+	unsigned int i, sum = 0;
+	for (i = 0; i < man->frees; i++) {
+		sum += man->free[i].size;
+	}
+	return sum;
+}
+
+// 分配内存
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size) {
+	unsigned int i, a;
+	for (i = 0; i < man->frees; i++) {
+		if (man->free[i].size >= size) {
+			a = man->free[i].addr;
+			man->free[i].addr += size;
+			man->free[i].size -= size;
+			if (man->free[i].size == 0) {
+				man->frees--;
+				for (; i < man->frees; i++) {
+					man->free[i] = man->free[i + 1];
+				}
+			}
+			return a;
+		}
+	}
+	return 0;
+}
+
+// 释放内存
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size) {
+	int i, j;
+	for (i = 0; i < man->frees; i++) {
+		if (man->free[i].addr > addr) {
+			break;
+
+		}
+	}
+
+	// free[i - 1].addr + free[i - 1].size == addr
+	// 不是第一个
+	if (i > 0) {
+		// 是否可以与前面的合并
+		if (man->free[i - 1].addr + man->free[i].size == addr) {
+			man->free[i - 1].size += size;
+			// 不是最后一个
+			if (i < man->frees) {
+				// 是否可以与后面的合
+				if (addr + size == man->free[i].addr) {
+					man->free[i - 1].size += man->free[i].size;
+					man->frees--;
+					for (; i < man->frees; i++) {
+						man->free[i] = man->free[i + 1];
+					}
+				}
+			}
+			return 0;
+		}
+	}
+	//不能与前面的合并
+	if (i < man->frees) {
+		// 判断是否能与后面的合并
+		if (addr + size == man->free[i].addr) {
+			man->free[i].addr = addr;
+			man->free[i].size += size;
+			return 0;
+		}
+	}
+	//不能与前面和后面合并
+	if (man->frees < MEMMAN_FREES) {
+		// 将free[i]以后的，向后移动，腾出一块空间
+		for (j = man->frees; j > i; j--) {
+			man->free[j] = man->free[j - 1];
+		}
+		man->frees++;
+		// 更新最大值
+		if (man->maxfrees < man->frees) {
+			man->maxfrees = man->frees;
+		}
+		man->free[i].addr = addr;
+		man->free[i].size = size;
+		return 0;
+	}
+
+	// 不能向后移动
+	man->losts++;
+	man->lostsize += size;
+	return -1;
+}
+
