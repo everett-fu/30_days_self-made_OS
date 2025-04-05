b100e21dac886f37be7ee72c2145a99ddbc82f71
everett.fu <everett.fu@gmail.com>
2025-04-05 16:55:10 +0800

    test:添加一个死循环，用于测试系统强制结束

---
+++ src/bug2.c
@@ -0,0 +1,18 @@
+/*
+ * Filename: bug2.c
+ * Author: everett.fu
+ * Created: 25-4-5
+ * Last Modified: 25-4-5
+ * Description:
+ * 这个文件是
+ *
+ *Functions:
+ * - test: 这是个测试函数
+ *
+ * Usage:
+ */
+
+void HariMain(void) {
+	for (;;) {
+	}
+}
