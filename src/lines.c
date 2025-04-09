25d3ce400bfc6dcd4322143738f1fd68f43e995c
everett.fu <everett.fu@gmail.com>
2025-04-09 20:25:05 +0800

    feat:添加绘制直线应用程序

---
+++ src/lines.c
@@ -0,0 +1,36 @@
+/*
+ * Filename: lines.c
+ * Author: everett.fu
+ * Created: 25-4-9
+ * Last Modified: 25-4-9
+ * Description:
+ * 这个文件是
+ *
+ *Functions:
+ * - test: 这是个测试函数
+ *
+ * Usage:
+ */
+int api_openwin(char *buf, int xsize, int ysize, int col_inv, char *title);
+void api_initmalloc(void);
+char *api_malloc(int size);
+void api_refreshwin(int win , int x0, int y0, int x1, int y1);
+void api_linewin(int win, int x0, int y0, int x1, int y1, int col);
+void api_end(void);
+
+int rand(void);
+
+void HariMain(void) {
+	char *buf;
+	int win;
+	api_initmalloc();
+	buf = api_malloc(160 * 100);
+	win = api_openwin(buf, 160, 100, -1, "lines");
+	int i;
+	for (i = 0; i < 8; i++) {
+		api_linewin(win + 1, 8, 26, 77, i * 9 + 26, i);
+		api_linewin(win + 1, 88, 26, i * 9 + 88, 89, i);
+	}
+	api_refreshwin(win, 6, 26, 154, 90);
+	api_end();
+}
